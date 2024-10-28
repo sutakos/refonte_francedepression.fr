2ad0004f5d678917a9efa54dc3f68d2284afe9a6
Mei-ling <165828868+Mei-lingWu@users.noreply.github.com>
2024-10-28 18:21:15 +0100

    Update commun.js

--- js/commun.js
+++ js/commun.js
@@ -5,13 +5,13 @@ window.onscroll = function() {
 
 //FONCTION QUI AFFICHE LA FLECHE QUAND ON ARRIVE A 90% DE LA PAGE
 function afficheBtn() {
-    const btn = document.getElementById("retourHaut");
+    const btn = document.getElementById('retourHaut');
     const tailleFenetre = window.innerHeight;
     const hauteurPage = document.body.scrollHeight;
     const defilement = window.scrollY + tailleFenetre; //window.scrollY RENVOIE LA DISTANCE EN PX A LAQUELLE L'UTILISATEUR A FAIT DEFILER LA PAGE DEPUIS LE HAUT
 
-    //SI DEFILEMENT >=90%
-    if (defilement >= hauteurPage * 0.9) {
+    //SI DEFILEMENT >=90% ET LARGEUR DE LA FENETRE >600
+    if (defilement >= hauteurPage * 0.9 && window.innerWidth > 600) {
         btn.style.display = "block"; //AFFICHE
     } else {
         btn.style.display = "none"; //CHACHE
@@ -23,20 +23,4 @@ function retourneHaut() {
     window.scrollTo({ top: 0, behavior: 'smooth' }); 
 }
 
-/* RESPONSIVE */
-
-const sidenav = document.getElementById("mySidenav");
-const openBtn = document.getElementById("openBtn");
-const closeBtn = document.getElementById("closeBtn");
-
-openBtn.onclick = openNav;
-closeBtn.onclick = closeNav;
-
-function openNav() {
-    sidenav.classList.add("active");
-}
-
-
-function closeNav() {
-    sidenav.classList.remove("active");
-}
+document.querySelector('#retourHaut').addEventListener('click',retourneHaut);
