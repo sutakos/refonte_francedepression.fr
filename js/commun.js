f1126ce7934be38bded7abdedc84f5874aba6afc
Lucas Lablanche <50883096+sutakos@users.noreply.github.com>
2024-10-28 18:34:33 +0100

    Update commun.js

--- js/commun.js
+++ js/commun.js
@@ -24,3 +24,21 @@ function retourneHaut() {
 }
 
 document.querySelector('#retourHaut').addEventListener('click',retourneHaut);
+
+/* RESPONSIVE */
+
+const sidenav = document.getElementById("mySidenav");
+const openBtn = document.getElementById("openBtn");
+const closeBtn = document.getElementById("closeBtn");
+
+openBtn.onclick = openNav;
+closeBtn.onclick = closeNav;
+
+function openNav() {
+    sidenav.classList.add("active");
+}
+
+
+function closeNav() {
+    sidenav.classList.remove("active");
+}
