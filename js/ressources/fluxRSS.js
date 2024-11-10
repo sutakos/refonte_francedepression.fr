e767ce67bb66027e24d595bc8cc5020bca6b4f8d
RefonteSite <lucie.phung@etu.u-paris.fr>
2024-11-10 23:34:24 +0100

    Dernière modif - Lucie

--- js/ressources/fluxRSS.js
+++ js/ressources/fluxRSS.js
@@ -4,3 +4,22 @@ elem.addEventListener('click',()=>{
     alert("Vous êtes maintenant abonné aux actualités ! (Il n'est pas accessible pour l'instant)")
 })
 
+/**
+ * @param {string} elementType
+ * @param {string} contenu
+ * @return {HTMLCollection}
+ */
+/**function creerMessage(elementType,contenu) {
+    const elem = document.createElement(elementType);
+    elem.textContent = contenu;
+    elem.style.opacity='50%'
+    elem.style.backgroundColor='blue'
+    elem.style.width='15em'
+    elem.style.height='8em'
+}
+elem.addEventListener('mouseover', () => {
+    const elemMessage = creerMessage(div,"Flux RSS\n Souscrire pour être informé(e) des mises à jour!")
+
+    document.body.append(elemMessage);
+    return elemMessage;
+})*/
