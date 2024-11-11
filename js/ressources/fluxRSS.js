e6be8d1fac7255b343694c20a4745fe497d5b9de
RefonteSite <lucie.phung@etu.u-paris.fr>
2024-11-12 00:08:04 +0100

    Modif du fluxRSS - Lucie

--- js/ressources/fluxRSS.js
+++ js/ressources/fluxRSS.js
@@ -1,6 +1,6 @@
-const elem = document.querySelector('#fluxRSS')
+const fluxRSS = document.querySelector('#fluxRSS')
 
-elem.addEventListener('click',()=>{
+fluxRSS.addEventListener('click',()=>{
     alert("Vous êtes maintenant abonné aux actualités ! (Il n'est pas accessible pour l'instant)")
 })
 
@@ -9,17 +9,45 @@ elem.addEventListener('click',()=>{
  * @param {string} contenu
  * @return {HTMLCollection}
  */
-/**function creerMessage(elementType,contenu) {
+function creerMessage(elementType,contenu) {
     const elem = document.createElement(elementType);
-    elem.textContent = contenu;
-    elem.style.opacity='50%'
-    elem.style.backgroundColor='blue'
-    elem.style.width='15em'
-    elem.style.height='8em'
-}
-elem.addEventListener('mouseover', () => {
-    const elemMessage = creerMessage(div,"Flux RSS\n Souscrire pour être informé(e) des mises à jour!")
+    elem.textContent = contenu
 
-    document.body.append(elemMessage);
-    return elemMessage;
-})*/
+    return elem;
+}
+/**
+ * @param {string} elementType
+ * @param {string} titre
+ * @return {HTMLCollection}
+ */
+function creerMessageTitre(elementType,titre){
+    const elem = document.createElement(elementType)
+    elem.textContent = titre
+    elem.style.fontWeight = 'bold'
+
+    return elem;
+}
+fluxRSS.addEventListener('mouseover', () => {
+    const elem = document.createElement('div')
+    const elemMessageTitre = creerMessageTitre('h4', "Flux RSS\n")
+    const elemMessage = creerMessage('p', " Souscrire pour être informé(e) des mises à jour!")
+    elem.style.position = 'absolute'
+    elem.style.border = '1px solid #8c8a8d'
+    elem.style.padding = '0.3em'
+    elem.style.borderRadius = '3%'
+    elem.style.backgroundColor = 'white'
+
+    elem.append(elemMessageTitre, elemMessage);
+
+    const rect = fluxRSS.getBoundingClientRect();
+    elem.style.left = `${rect.right + 10}px`;  // Position à droite de l'image
+    elem.style.top = `${rect.top}px`;          // Aligné en hauteur avec l'image
+
+    // Ajoute le conteneur au document
+    document.body.append(elem);
+
+    fluxRSS.addEventListener('mouseout', () => {
+        document.body.removeChild(elem);
+    }, { once: true });
+})
+
