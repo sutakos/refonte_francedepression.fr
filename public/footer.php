e3968b73ada6adec0cd687332d85ef99a3f8f585
Mei-ling <165828868+Mei-lingWu@users.noreply.github.com>
2024-12-29 12:31:17 +0100

    .html -> .php + ajout du form

--- public/footer.php
+++ public/footer.php
@@ -4,26 +4,28 @@
 			<div class="divFooter">
 
 				<div class="divLogo">
-					<a href="index.html"><img src="images/commun/logoSF.PNG" width="150px" alt="Logo"></a>
+					<a href="index.php"><img src="images/commun/logoSF.PNG" width="150px" alt="Logo"></a>
 				</div>
 
 				<div>
 					<span class="titre-footer">Vous êtes concerné(e) ?</br></br></span>
 					<p class="contenu">	Ecoute patients : 07 84 96 88 28 </br></br>
-France Dépression – 10 rue de Norvège </br> – 54500 Vandœuvre 
+France Dépression – 10 rue de Norvège </br> – 54500 Vandœuvre
 </p></div>
 
 				<div>
 					<span class="titre-footer">À propos</br></br></span>
-					<p class="contenu"><a href="../quiSommesNous.html">L'association</a></br></br>
-						<a href="Antennes.html">Antennes</a>
+					<p class="contenu">
+                        <a href="quiSommesNous.php">L'association</a></br></br>
+						<a href="antennes.php">Antennes</a></br></br>
+                        <a href="questionnaire.php">Questionnaire</a>
 					</p>
 				</div>
 
 				<div>
 					<span class="titre-footer">Contacts</br></br></span>
 					<p class="contenu">06 03 89 34 47 </br></br>
-						contact@francedepression.fr 
+						contact@francedepression.fr
 					</p>
 				</div>
 
